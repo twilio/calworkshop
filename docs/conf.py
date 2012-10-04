f6f2732b9c3180f4e1fbab72b7ed8525a15ce579
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-10-04 14:28:24 -0700

    Don't depend on the spellcheck module

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,11 @@ sys.path.append(os.path.abspath('_themes'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'sphinxcontrib.spelling' ]
+try:
+    import sphinxcontrib.spelling
+    extensions = ['sphinxcontrib.spelling']
+except ImportError:
+    extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
