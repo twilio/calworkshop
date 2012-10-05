3a250ae06d7ef605d3208867c7d3fe3630194601
VMDX <vmdx.doom@gmail.com>
2012-10-05 14:55:56 -0700

    bugfixes on make_call.py. incorrect python client call and invalid twiml url

--- make_call.py
+++ make_call.py
@@ -5,7 +5,8 @@ TWILIO_AUTH_TOKEN = ''
 
 TO_NUMBER = ''       # Your verified phone number
 FROM_NUMBER = ''     # Your Twilio phone number
-TWIML_URL = 'http://twimlets.com/message?Message[1]=Hello+World'
+TWIML_URL = 'http://twimlets.com/message?Message=Hello+World'
 
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-client.calls.messages.create(to=TO_NUMBER, from_=FROM_NUMBER, url=TWIML_URL)
+client.calls.create(to=TO_NUMBER, from_=FROM_NUMBER, url=TWIML_URL)
+
